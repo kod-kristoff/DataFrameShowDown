9db42f776844626ef3b2c0f520c7d4d8e1ec8e28
Kristoffer Andersson <kod.kristoff@gmail.com>
2023-01-22 18:02:56 +0100

    fix: correct data path

--- RustDataFusion/main.rs
+++ RustDataFusion/main.rs
@@ -3,20 +3,25 @@ use std::time::Instant;
 
 #[tokio::main]
 async fn main() -> datafusion::error::Result<()> {
-  let now = Instant::now();
-  let ctx = SessionContext::new();
-  ctx.register_csv("trips", "data/*.csv", CsvReadOptions::new()).await?;
+    let now = Instant::now();
+    let ctx = SessionContext::new();
+    ctx.register_csv("trips", "../data/*.csv", CsvReadOptions::new())
+        .await?;
 
-  // create a plan to run a SQL query
-  let df = ctx.sql("
+    // create a plan to run a SQL query
+    let df = ctx
+        .sql(
+            "
   SELECT COUNT('transaction_id') as cnt,
     date_part('month', to_timestamp(started_at)) as month
   FROM trips
   GROUP BY date_part('month', to_timestamp(started_at))
-  ").await?;
+  ",
+        )
+        .await?;
 
-  df.show_limit(100).await?;
-  let elapsed = now.elapsed();
-  println!("Elapsed: {:.2?}", elapsed);
-  Ok(())
+    df.show_limit(100).await?;
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+    Ok(())
 }
